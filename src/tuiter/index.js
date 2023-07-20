73deac455502c5db79cc9347fa5e397880270c22
yunke-l <yunkeli.work@gmail.com>
2023-07-20 02:01:18 -0700

    JavaScript and React.js Assignment 3

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,10 +1,33 @@
+import { Routes, Route } from "react-router";
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import HomeScreen from "./home-screen";
+import ExploreScreen from "./explore-screen/index.js";
+import BookmarksScreen from "./bookmarks-screen";
+import ProfileScreen from "./profile-screen";
+import WhoToFollowList from "./who-to-follow-list";
+
 function Tuiter() {
-  return(
+  return (
       <div>
-        <Nav/>
-        <h1>Tuiter</h1>
+        <Nav />
+        <div className="row">
+          <div className="col-2">
+            <NavigationSidebar />
+          </div>
+          <div className="col-7">
+            <Routes>
+              <Route path="/home" element={<HomeScreen />} />
+              <Route path="/explore" element={<ExploreScreen />} />
+              <Route path="/bookmarks" element={<BookmarksScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+            </Routes>
+          </div>
+          <div className="col-3">
+            <WhoToFollowList />
+          </div>
+        </div>
       </div>
   );
 }
-export default Tuiter
+export default Tuiter;
