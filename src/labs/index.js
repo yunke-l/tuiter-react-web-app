f437450cd6907f45ecf25b9598b3a43b4c697da0
yunke-l <yunkeli.work@gmail.com>
2023-07-24 20:51:11 -0700

    Finished lab3

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,11 +1,17 @@
 import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import {Routes, Route} from "react-router";
 import Nav from "../nav";
+
 
 function Labs() {
   return (
       <div>
         <Nav/>
-        <Assignment3/>
+        <Routes>
+          <Route path="a3" element={<Assignment3/>}/>
+          <Route path="a4" element={<Assignment4/>}/>
+        </Routes>
       </div>
   );
 }
