7d555c0a060c11644939de53a3d6fcd3f94d14bb
yunke-l <yunkeli.work@gmail.com>
2023-08-06 22:37:55 -0700

    change server api base to render in auth-service

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+const SERVER_API_URL = process.env.REACT_APP_API_BASE;
 const USERS_URL = `${SERVER_API_URL}/users`;
 
 const api = axios.create({ withCredentials: true });
--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-// const API_BASE = 'https://tuiter-node-server-app-ykl-6d243b7ccf0d.herokuapp.com/api';
-const API_BASE = 'https://tuiter-node-server-app-4uls.onrender.com/api';
-// const API_BASE = process.env.REACT_APP_API_BASE;
+// const TUITS_API = 'https://tuiter-node-server-app-ykl-6d243b7ccf0d.herokuapp.com/api/tuits';
+// const TUITS_API = 'https://tuiter-node-server-app-4uls.onrender.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
 
