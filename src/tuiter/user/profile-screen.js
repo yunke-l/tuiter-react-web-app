fca7caf7da8824dc44a1fec90eca13a404005435
yunke-l <yunkeli.work@gmail.com>
2023-08-15 16:35:42 -0700

    profile-screen

--- src/tuiter/user/profile-screen.js
+++ src/tuiter/user/profile-screen.js
@@ -15,10 +15,12 @@ function ProfileScreen() {
     setProfile(newProfile);
   };
 
-  const save = () => {
-    dispatch(updateUserThunk(profile)).then(() => {
-      setSavedProfile(profile);
-    });
+  const save = async () => {
+    const updateResponse = await dispatch(updateUserThunk(profile));
+    const fetchResponse = await dispatch(profileThunk());
+    if (updateResponse.payload && fetchResponse.payload) {
+      setProfile(fetchResponse.payload);
+    }
   };
 
   useEffect(() => {
@@ -27,7 +29,7 @@ function ProfileScreen() {
       setProfile(payload);
     };
     fetchProfile();
-  }, [dispatch]);
+  }, []);
 
   return (
       <div>
